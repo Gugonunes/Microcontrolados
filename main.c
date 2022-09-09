34bc17315e0976bfc2c445522d837cd93c576b6d
Gustavo Alexandre Tuchlinowicz Nunes <67166518+Gugonunes@users.noreply.github.com>
2022-09-08 22:58:38 -0300

    Interrupção alterada para P1

--- main.c
+++ main.c
@@ -1,57 +1,73 @@
-#include <msp430.h>
+
+#include <msp430.h> 
 unsigned long int i = 0;
 void main(void) {
 
     WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
-    //seta tudo como saida
-    P1DIR = BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
-    //todas em baixo nivel
-    P1OUT = 0x00;
+    __enable_interrupt();
+    //seta tudo como saída
+    P1DIR = ~(BIT0 + BIT1);
+    P1OUT = BIT0 + BIT1;
+    P1REN = BIT0 + BIT1;
+    P1IES = BIT0 + BIT1;
+    P1IFG = 0;
+    P1IE = BIT0 + BIT1;
 
-    //seta tudo como saida
-    P2DIR = BIT0 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
-    //todas em baixo nivel
-    P2OUT = BIT1 + BIT2;
-    P2REN = BIT1 + BIT2;
-    P2IES = BIT1 + BIT2;
+    //seta tudo como saída
+    P2DIR = BIT0 + BIT1 + BIT2 +BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+    //todas em baixo nível
+    P2OUT = 0x00;
     P2IFG = 0;
-    P2IE = BIT1 + BIT2;
 
     while(1){
-        P1OUT = BIT0 + BIT5 + BIT6;
-        for(i = 0; i < 1600000; i++);  //~ 8s
-        P1OUT &= ~BIT0;
+        P1OUT = BIT5 + BIT6;
+        P2OUT = BIT1;
+        for(i = 0; i < 800000; i++);  //~ 8s
+        P2OUT &= ~BIT1;
 
-        P1OUT ^= BIT1;
-        for(i = 0; i < 400000; i++);  //~ 2s
-        P1OUT &= ~(BIT1 + BIT5 + BIT6);
+        P2OUT ^= BIT2;
+        for(i = 0; i < 200000; i++);  //~ 2s
+        P1OUT &= ~(BIT5 + BIT6);
+        P2OUT &= ~BIT2;
 
         P1OUT = BIT2 + BIT3 + BIT7;   //~ 8s
-        for(i = 0; i < 1600000; i++);
+        for(i = 0; i < 800000; i++);
         P1OUT &= ~BIT3;
 
         P1OUT ^= BIT4;
-        for(i = 0; i < 400000; i++);  //~ 2s
+        for(i = 0; i < 200000; i++);  //~ 2s
         P1OUT &= ~(BIT3 + BIT4 + BIT7);
     };
 
 }
-//TODO arrumar a interrupçao
-#pragma vector = PORT2_VECTOR //interrupção ainda precisa de ajustes
-__interrupt void Port2_RTI(void) {
-    switch(P1IFG & (BIT1 + BIT2)){
+
+#pragma vector = PORT1_VECTOR
+__interrupt void Port1_RTI(void) {
+    switch(P1IFG & (BIT0 + BIT1)){
+    case BIT0:
+        P1IFG &= ~BIT0;
+
+        if((P2OUT == BIT1) && (P1OUT == BIT5 + BIT6)){
+            P1OUT ^= BIT2 + BIT6;
+            P2OUT ^= BIT1;
+            for(i = 0; i < 400000; i++);  //~ 2s
+            P1OUT ^= BIT2 + BIT6;
+            P2OUT ^= BIT1;
+        }
+
+        break;
     case BIT1:
         P1IFG &= ~BIT1;
-        P1OUT ^= BIT7;
-        break;
-    case BIT2:
-        P1IFG &= ~BIT2;
+
+        if(P1OUT == BIT2 + BIT3 + BIT7){
+            P1OUT ^= BIT3 + BIT5 + BIT7;
+            for(i = 0; i < 400000; i++);  //~ 2s
+            P1OUT ^= BIT3 + BIT5 + BIT7;
+        }
 
         break;
     default:
-        P1IFG &= ~(BIT1 + BIT2);
+        P1IFG &= ~(BIT0 + BIT1);
         break;
     }
 }
-
-
